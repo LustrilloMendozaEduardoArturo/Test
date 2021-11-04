a77e716d9a9afff22b13c8cc59eaaa38ba5e362b
LustrilloMendozaEduardoArturo <Lustrillomendozaeduardoarturo@gmail.com>
2021-11-04 15:12:15 -0600

    modificacion2

--- HolaMundo3.java
+++ HolaMundo3.java
@@ -2,7 +2,8 @@ public class HolaMundo3
 {
         public void main( String a[])
         {
-                System.out.println( "hola mundo2 " );
+                System.out.println( "hola mundo" );
 		System.out.println("Hola ANdy" );
-        }
+        	System.out.println("Hola Lalo" );
+	}
 }
